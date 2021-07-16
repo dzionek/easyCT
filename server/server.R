ab9af62f52f69337921301f2365c5c1d5fce44a7
Bartosz Dzionek <vburtoch@gmail.com>
2021-07-16 20:21:21 +0100

    Add selecting folder to layout

--- server/server.R
+++ server/server.R
@@ -1,6 +1,16 @@
 # Back-end part of the application.
 
-server <- function(input, output) {
+library(shiny)
+library(fs)
+
+server <- function(input, output, session) {
+  volumes <- c(Home = fs::path_home(), "R Installation" = R.home(), getVolumes()())
+  
+  shinyDirChoose(
+    input, "directory", roots = volumes, session = session,
+    restrictions = system.file(package = "base"), allowDirCreate = FALSE
+  )
+  
   set.seed(122)
   histdata <- rnorm(500)
   output$plot1 <- renderPlot({
--- ui/ui.R
+++ ui/ui.R
@@ -1,12 +1,23 @@
 # Front-end part of the application
 
+library(shiny)
 library(shinydashboard)
+library(shinyFiles)
 
 source("ui/tabs/time_tab.R")
 source("ui/tabs/classifier_tab.R")
+source("ui/tabs/info_tab.R")
 
 sidebar <- dashboardSidebar(
   sidebarMenu(
+    h4("Select a folder with photos", align = "center"),
+    fluidRow(column(
+      12, align = "center",
+      shinyDirButton("directory", "Folder select", "Please select a folder")
+    )),
+    hr(),
+    
+    info_tab.menu,
     time_tab.menu,
     classifier_tab.menu,
     menuItem("GitHub", icon = icon("github"),
@@ -17,7 +28,8 @@ sidebar <- dashboardSidebar(
 body <- dashboardBody(
   tabItems(
     time_tab.body,
-    classifier_tab.body
+    classifier_tab.body,
+    info_tab.body
   )
 )
 
