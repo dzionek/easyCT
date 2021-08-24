5ca2078db9df6880fb49d0e4a84d938304a0f00a
Bartosz Dzionek <vburtoch@gmail.com>
2021-08-24 14:59:28 +0200

    Add blank tabboxes for import/export

--- app/ui/tabs/export_tab.R
+++ app/ui/tabs/export_tab.R
@@ -2,10 +2,33 @@
 
 library(shinydashboard)
 
-export_tab.menu <- menuItem("Export", tabName = "export", icon = icon("file-download"))
+export_tab.menu <- menuItem("Import & Export", tabName = "export", icon = icon("file-download"))
 
 export_tab.body <- tabItem(
   tabName = "export",
-  h2("EXPORT"),
-  p("This tab is used to export your models and results from the application.")
+  h1("Import & Export"),
+  p("This tab is used to import and export files between this application and
+    your machine."),
+  br(),
+  
+  h2("Time Tab"),
+  fluidRow(
+    tabBox(title = "Selected photos", side = "right",
+           # tabPanel("Import", p("Currently not supported"))
+           tabPanel("Export", p("Export here"))
+    )
+  ),
+
+  h2("Classifier Tab"),
+  fluidRow(
+    tabBox(title = "Model", side = "right",
+           tabPanel("Import", p("Import here")),
+           tabPanel("Export", p("Export here"))
+    ),
+    
+    tabBox(title = "Classification results", side = "right",
+           # tabPanel("Import", p("Currently not supported"))
+           tabPanel("Export", p("Export here"))
+    )
+  ),
 )
